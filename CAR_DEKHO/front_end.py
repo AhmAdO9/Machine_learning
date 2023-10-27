3dff33d888d53aa428b47b6b96ddb0561c3ff957
faheem <adahm7114@gmail.com>
2023-10-27 18:32:22 +0530

    _

--- CAR_DEKHO/front_end.py
+++ CAR_DEKHO/front_end.py
@@ -21,4 +21,31 @@ def predict():
         Kms_Driven2 = np.log(Kms_Driven)
         Fuel_Type_Petrol = request.form['Fuel_Type_Petrol']
         Owner = int(request.form['Owner'])
-        
+        if(Fuel_Type_Petrol == 'Petrol'):
+            Fuel_Type_Petrol=1
+            Fuel_Type_Diesel=0
+        else:
+            Fuel_Type_Petrol=0
+            Fuel_Type_Diesel=1
+        Year = 2020-Year
+        Seller_Type_Individual = request.form['Seller_Type_Individual']
+        if Seller_Type_Individual == 'Individual':
+            Seller_Type_Individual = 1
+        else:
+            Seller_Type_Individual=0
+        Transmission_Manual=request.form['Transmission_Manual']
+        if Transmission_Manual == 'Manual':
+            Transmission_Manual = 1
+        else:
+            Transmission_Manual = 0
+        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Manual]])
+        output=round(prediction[0],2)
+        if output<0:
+            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
+        else:
+            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
+    else:
+        return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
