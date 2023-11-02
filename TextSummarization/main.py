0885f3908a8437ae06a2703361cb0b7d9f7567d4
faheem <adahm7114@gmail.com>
2023-11-02 22:24:57 +0530

    _

--- TextSummarization/main.py
+++ TextSummarization/main.py
@@ -1,6 +1,7 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from logger import logging
 from exception import CustomException
 import sys
@@ -36,6 +37,18 @@ try:
    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
    data_transformation = DataTransformationTrainingPipeline()
    data_transformation.main()
+   logging.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<< \n \n x==============x")
+except Exception as e:
+    raise CustomException(e, sys)
+
+
+
+STAGE_NAME = "Model Trainer" 
+
+try: 
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
    logging.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<< \n \n x==============x")
 except Exception as e:
     raise CustomException(e, sys)
